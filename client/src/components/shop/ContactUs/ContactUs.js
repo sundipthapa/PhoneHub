88cef31515953275282aebf4c0985311fe0d0cd8
sundipthapa <lakhanmgr123@gmail.com>
2024-07-30 16:18:07 +0545

    contact us

---
+++ client/src/components/shop/ContactUs/ContactUs.js
@@ -0,0 +1,160 @@
+// Make a contact us page
+
+import React, { Fragment } from "react";
+import { Navber } from "../partials";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+ const apiURL = 'http://localhost:8000'
+const ContactUs = () => {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name,
+      email,
+      message,
+    };
+    console.log(data);
+    try {
+      if (name === "" || email === "" || message === "") {
+        alert("Please fill all the fields");
+        return;
+      }
+      const re = /\S+@\S+\.\S+/;
+      if (!re.test(email)) {
+        alert("Please enter valid email");
+        return;
+      }
+      fetch(`${apiURL}/api/contact-us`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          toast("✅ Your Message Sent Successfully", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        });
+    } catch (error) {
+      toast("❌ Something Went Wrong", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Navber></Navber>
+      <Fragment>
+        <div className="container mx-auto my-auto">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-3xl font-bold text-gray-800">Contact Us</h1>
+            <p className="text-gray-600">
+              We are here to help and answer any question you might have. We
+              look forward to hearing from you.
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full md:w-1/2 lg:w-1/3">
+              <form
+                className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+                onSubmit={handleSubmit}
+              >
+                <div className="mt-4 mb-4">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="name"
+                  >
+                    Name
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="name"
+                    type="text"
+                    placeholder="Name"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="email"
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="message"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="message"
+                      type="text"
+                      placeholder="Message"
+                      onChange={(e) => setMessage(e.target.value)}
+                    />
+
+                    <div className="flex items-center justify-between mt-4">
+                      <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                      >
+                        Send
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    </>
+  );
+};
+
+export default ContactUs;
---
+++ client/src/components/shop/index.js
@@ -0,0 +1,32 @@
+import Home from "./home";
+import WishList from "./wishlist";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import AdminProtectedRoute from "./auth/AdminProtectedRoute";
+import CartProtectedRoute from "./auth/CartProtectedRoute";
+import { LayoutContext } from "./layout";
+import { layoutState, layoutReducer } from "./layout/layoutContext";
+import { isAdmin, isAuthenticate } from "./auth/fetchApi";
+import PageNotFound from "./layout/PageNotFound";
+import ProductDetails from "./productDetails";
+import ProductByCategory from "./home/ProductByCategory";
+import CheckoutPage from "./order/CheckoutPage";
+import ContactUs from "./ContactUs/ContactUs";
+import ResetPasswordPage from "./auth/ResetPassword";
+export {
+  Home,
+  WishList,
+  ProtectedRoute,
+  AdminProtectedRoute,
+  CartProtectedRoute,
+  LayoutContext,
+  layoutState,
+  layoutReducer,
+  isAdmin,
+  isAuthenticate,
+  PageNotFound,
+  ProductDetails,
+  ProductByCategory,
+  CheckoutPage,
+  ContactUs,
+  ResetPasswordPage
+};
